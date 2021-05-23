5db8af3181b425623364100dfccb71fca4305055
lightmanLP <liteman1000@gmail.com>
2021-05-23 17:51:40 +0700

    update cli
    + import/export

--- utmo/cli.py
+++ utmo/cli.py
@@ -1,3 +1,7 @@
+from typing import Optional
+from pathlib import Path
+import pickle
+
 import click
 
 from . import models, structures, adapters
@@ -14,14 +18,14 @@ def cli():
     pass
 
 
-@click.command("list", help="show list of songs")
+@cli.command("list", help="show list of songs")
 def list_songs():
     songs = models.session.query(models.Song).all()
     for song in songs:
         print(f"{song.id: >4}: {song}")
 
 
-@click.command("add", help="add song from url to db")
+@cli.command("add", help="add song from url to db")
 # @click.option("--provider", default=None, type=structures.Providers)
 @click.argument("url", type=str)
 def add_song(url: str):
@@ -37,7 +41,7 @@ def add_song(url: str):
     models.session.commit()
 
 
-@click.command("play", help="play music")
+@cli.command("play", help="play music")
 @click.argument("song_id", type=int, metavar="id")
 def play_song(song_id: int):
     song = models.session.query(models.Song).get(song_id)
@@ -45,22 +49,45 @@ def play_song(song_id: int):
     adapters.system.play_by_url(url)
 
 
-@click.command("stats", help="view organizer stats")
+@cli.command("stats", help="view organizer stats")
 def organizer_stats():
     ...  # TODO
 
 
-@click.command("export", help="export songs")
-def export_songs():
+@cli.command("export", help="export songs")
+@click.option("-o", "--output", default="songs.pickle", type=click.Path())
+@click.option("-p", "--save-plays", is_flag=True)
+@click.option("-l", "--export-locals", is_flag=True)
+def export_songs(
+    output: str,
+    save_plays: bool,
+    export_locals: bool
+):
+    output = Path(output)
+    data = models.Song.export_songs(save_plays, export_locals)
+    output.write_bytes(pickle.dumps(data))
+    print("export done")  # FIXME: rework echo
+
+
+@cli.command("import", help="import songs")
+@click.argument("file", type=click.Path(exists=True))
+def import_songs(file: str):
+    file = Path(file)
+    data = pickle.loads(file.read_bytes())
+    models.Song.import_songs(data)
+    print("import done")  # FIXME: rework echo
+
+
+@cli.command("search", help="search for songs by tags and names")
+def search_song():
     ...  # TODO
 
 
-@click.command("import", help="import songs")
-def import_songs():
+@cli.command("remove", help="removes songs")
+def remove_songs(*ids: int):  # FIXME
     ...  # TODO
 
 
-cli.add_command(list_songs)
-cli.add_command(add_song)
-cli.add_command(play_song)
-cli.add_command(organizer_stats)
+@cli.command("edit", help="edit song data")
+def edit_song():
+    ...  # TODO
