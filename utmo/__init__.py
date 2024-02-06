5e376916b326645b38da26ac2a1e92eff3d97343
lightmanLP <liteman1000@gmail.com>
2024-02-06 16:30:43 +0700

    cleanup

--- utmo/__init__.py
+++
@@ -1,8 +0,0 @@
-from .tools import Extractor, Scrapper
-from .dynamic_storage import VKManager
-from .adapters import (
-    control as control_adapter,
-    system as system_adapter
-)
-from .structures import Provider, Platform
-from .config import config
